ffe626a5f9eeb37223d20fa5c3871afc7b2dad1a
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2022-12-26 18:12:17 +0800

    it works

--- SAC/overrides/kubejs/server_scripts/overworld.js
+++ SAC/overrides/kubejs/server_scripts/overworld.js
@@ -2,8 +2,7 @@ onEvent('player.inventory.changed', event => {
 	if (event.item.id == 'mowziesmobs:wrought_axe' && !event.player.stages.has('overworld')) {
         event.player.stages.add('overworld')
         event.server.scheduleInTicks(100, event.server, function (callback) {
-            callback.server.runCommandSilent('execute in overworld run tp ' + event.player.name + ' 0 256 0')
-            callback.server.runCommandSilent('effect give ' + event.player.name + ' slow_falling 30 0')
+            callback.server.runCommandSilent('school leave ' + event.player.name)
         })
     }
 })
