102919328138082c551f572c0196c21b03396940
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2023-02-28 20:47:42 +0800

    Delete SAC directory

--- SAC/overrides/kubejs/server_scripts/overworld.js
+++
@@ -1,8 +0,0 @@
-onEvent('player.inventory.changed', event => {
-	if (event.item.id == 'mowziesmobs:wrought_axe' && !event.player.stages.has('overworld')) {
-        event.player.stages.add('overworld')
-        event.server.scheduleInTicks(100, event.server, function (callback) {
-            callback.server.runCommandSilent('school leave ' + event.player.name)
-        })
-    }
-})
